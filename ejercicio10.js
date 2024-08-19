e979ebeb268e283bf487395d93cc9c0657222e29
Angel <111787331+angel6464@users.noreply.github.com>
2024-08-18 21:18:25 -0600

    Guia Completa

--- ejercicio10.js
+++ ejercicio10.js
@@ -9,7 +9,7 @@ Lo que queremos devolver:
 • Mostrar por pantalla un mensaje que indique cuál de los tres turnos tiene un
 promedio de edades mayor. */
 
-const turnoMañana = [16, 17, 16, 15, 14]; // Aquí puedes poner los valores que desees
+const turnoMañana = [16, 17, 16, 15, 14]; // Aquí se pueden colocar los valores que se desean
 const turnoTarde=[11, 10, 12, 14, 13, 12];
 const turnoNoche=[35, 35, 30, 24, 25, 21, 22, 27, 19, 34, 20];
 
@@ -17,17 +17,18 @@ const turnoNoche=[35, 35, 30, 24, 25, 21, 22, 27, 19, 34, 20];
 let contadorMañana = 0;
 let contadorTarde = 0;
 let contadorNoche = 0;
-let sumaPares = 0;
 
 //for para el arreglo del turno de la Mañana
 for (let i = 0; i < turnoMañana.length; i++) {
     contadorMañana=contadorMañana+turnoMañana[i];
 }
 
+//for para el arreglo del turno de la tarde
 for (let i = 0; i < turnoTarde.length; i++) {
     contadorTarde=contadorTarde+turnoTarde[i];
 }
 
+//for para el arreglo del turno de la noche
 for (let i = 0; i < turnoNoche.length; i++) {
     contadorNoche=contadorNoche+turnoNoche[i];
 }
@@ -36,14 +37,12 @@ let promedioMañana=contadorMañana/turnoMañana.length;
 let promedioTarde=contadorTarde/turnoTarde.length;
 let promedioNoche=contadorNoche/turnoNoche.length;
 
-//Imprimir los promedios
-
+//Muestra los promedios
 console.log("El promedio de edades del turno de la mañana es: ", promedioMañana);
 console.log("El promedio de edades del turno de la tarde es: ", promedioTarde);
 console.log("El promedio de edades del turno de la noche es: ", promedioNoche);
 
 //Para saber cual es el turno con el promedio mayor
-
 if(promedioMañana>promedioTarde && promedioMañana>promedioNoche){
     console.log("El turno de la mañana tiene un promedio de edades mayor.");
 }else if(promedioTarde>promedioMañana && promedioTarde>promedioNoche){
